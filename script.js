8e830c9e0ebc55c07697973596971fbedd0daf86
BerksGamesMC <berksgamesyt@gmail.com>
2025-07-25 16:16:39 +0300

    script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("downloadBtn").addEventListener("click", function() {
+  window.open("https://www.mediafire.com/file/owzm8q996lntkih/StealABrainrotUncopyLocked.rbxl/file", "_blank");
+});
